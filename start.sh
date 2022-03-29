1357212701ca4909f25b71c74cf5cf6c81f5c129
= <=>
2022-03-29 17:12:53 +0200

    gestion de curl

--- start.sh
+++ start.sh
@@ -1,12 +1,36 @@
 # variables
 FILE=dotnet-install.sh
 URL=https://dot.net/v1/dotnet-install.sh
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+# checking curl installation
+if ! command -v curl &> /dev/null
+then
+    if command -v apt &> /dev/null
+    then
+        # installation with apt
+        sudo apt install curl
+    elif command -v pacman &> /dev/null
+    then
+        # install with pacman
+        sudo pacman -S curl
+    else
+        echo -e "${RED}You must install curl."
+        exit
+    fi
+fi
 
 echo "Downloading $FILE from $URL"
-curl -L https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+curl -L $URL > $FILE
 
 echo "According right"
-chmod +x dotnet-install.sh
+sudo chmod +x $FILE
 
 echo "Installation.."
-./dotnet-install.sh --install-dir /usr/share/dotnet -channel Current -version 5.0.100 
+sudo ./$FILE --install-dir /usr/share/dotnet -channel Current -version 5.0.100
+
+echo "Removing file..."
+rm ./$FILE
+
+echo -e "${GREEN}Dotnet 5 has been successfuly installed !"
