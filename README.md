c7749717d7e2b46748e856c803dbfd7c8e5d0596
= <=>
2022-03-30 18:34:45 +0200

    V1.0
    Manage zsh and bash shell to add global path

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 
 # Dotnet 5 linux installation
+Simple repo to download dotnet 5 on linux
 
+## Usage
 You can start `start.sh` program. by using. 
 ```
-(sudo) ./start.sh
+sudo ./start.sh
 ```
+
+It will install **Dotnet 5.0.100** and configure the global path if needed.
+
+## Specifications
+⚠ For adding global path. For the moment, only **bash** and **zsh** shells are compatible.
+
+## Contact
+For any suggestion or remark, you can contact me via [Discord](https://discord.com/users/498432113666818058).  
--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 NC='\033[0m' # No Color
 
+INSTALLATION_PATH="/usr/share/dotnet"
+
 # checking curl installation
 if ! command -v curl &> /dev/null
 then
@@ -33,10 +35,41 @@ curl -L $URL > $FILE
 echo -e "${GREEN}Make $FILE as executable file..."
 sudo chmod +x $FILE
 
-echo "Installation.."
-sudo ./$FILE --install-dir /usr/share/dotnet -channel Current -version 5.0.100
+echo -e "Installation.."
+sudo ./$FILE --install-dir $INSTALLATION_PATH -channel Current -version 5.0.100
 
-echo "Removing file..."
+echo -e "Removing file..."
 rm ./$FILE
+
+# install add global path
+add_global_path=0
+read -p "Trying to add global path ? [Y/n] " yn
+case $yn in
+    [Yy]* ) add_global_path=1; break;;
+    [Nn]* ) add_global_path=0; break;;
+    * ) add_global_path=1;;
+esac
+
+# environment
+ENVFILE=""
+case $SHELL in
+    */zsh) 
+        # append path
+        ENVFILE="zshrc" ;;
+    */bash)
+        ENVFILE="bashrc" ;;
+    *) 
+esac
+
+if [ $add_global_path -eq 1 ]
+then
+    if [[ "$ENVFILE" != "" ]]
+    then
+        echo -n 'export PATH=/usr/share/dotnet:$PATH' >> ~/.$ENVFILE
+        echo -e "${GREEN}$INSTALLATION_PATH has been add to path."
+    else
+        echo -e "${ORANGE}Didn't found any compatible shell. Please add this global path to your environment: $INSTALLATION_PATH"
+    fi
+fi
 
 echo -e "${GREEN}Dotnet 5 has been successfuly installed !"
